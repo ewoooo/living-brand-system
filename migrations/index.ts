ee58b213daed0aa0c916375fa2d639b39c20c404
glyphszyme <47078648+glyphszyme@users.noreply.github.com>
2026-07-28 12:02:24 +0900

    chore: colorPairingRecommendation 블록 마이그레이션 추가
    새 블록 테이블·enum 생성(순수 CREATE, 증분). PR/CI/공유DB 적용용 핸드오프 마이그레이션.

--- migrations/index.ts
+++ migrations/index.ts
@@ -4,6 +4,7 @@ import * as migration_20260723_082743_logo_viewer_topics from './20260723_082743
 import * as migration_20260723_085557_logo_viewer_real_height from './20260723_085557_logo_viewer_real_height';
 import * as migration_20260724_005136_logo_group_viewer from './20260724_005136_logo_group_viewer';
 import * as migration_20260724_074150_add_color_pairing_block from './20260724_074150_add_color_pairing_block';
+import * as migration_20260728_023011_add_color_pairing_recommendation_block from './20260728_023011_add_color_pairing_recommendation_block';
 
 export const migrations = [
   {
@@ -34,6 +35,11 @@ export const migrations = [
   {
     up: migration_20260724_074150_add_color_pairing_block.up,
     down: migration_20260724_074150_add_color_pairing_block.down,
-    name: '20260724_074150_add_color_pairing_block'
+    name: '20260724_074150_add_color_pairing_block',
+  },
+  {
+    up: migration_20260728_023011_add_color_pairing_recommendation_block.up,
+    down: migration_20260728_023011_add_color_pairing_recommendation_block.down,
+    name: '20260728_023011_add_color_pairing_recommendation_block'
   },
 ];
