8726fdf5d950ba02d4ed4ef941684a574bf11ccb
Wooseong <leews7752@gmail.com>
2026-08-13 20:19:25 +0900

    style: 출력 정책 마이그레이션 공백 정리

--- migrations/20260813_075510_studio_export_policy.ts
+++ migrations/20260813_075510_studio_export_policy.ts
@@ -17,17 +17,17 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   ALTER TABLE "image_profiles" RENAME COLUMN "output_original" TO "export_policy_original";
   ALTER TABLE "_image_profiles_v" RENAME COLUMN "version_output_original" TO "version_export_policy_original";
   ALTER TABLE "image_profiles_export_policy_allowed_formats" DROP CONSTRAINT "image_profiles_output_allowed_formats_parent_fk";
-  
+
   ALTER TABLE "_image_profiles_v_version_export_policy_allowed_formats" DROP CONSTRAINT "_image_profiles_v_version_output_allowed_formats_parent_fk";
-  
+
   ALTER TABLE "graphic_profiles_export_policy_allowed_formats" DROP CONSTRAINT "graphic_profiles_output_allowed_formats_parent_fk";
-  
+
   ALTER TABLE "_graphic_profiles_v_version_export_policy_allowed_formats" DROP CONSTRAINT "_graphic_profiles_v_version_output_allowed_formats_parent_fk";
-  
+
   ALTER TABLE "templates_export_policy_allowed_formats" DROP CONSTRAINT "templates_output_allowed_formats_parent_fk";
-  
+
   ALTER TABLE "_templates_v_version_export_policy_allowed_formats" DROP CONSTRAINT "_templates_v_version_output_allowed_formats_parent_fk";
-  
+
   DROP INDEX "image_profiles_output_allowed_formats_order_idx";
   DROP INDEX "image_profiles_output_allowed_formats_parent_idx";
   DROP INDEX "_image_profiles_v_version_output_allowed_formats_order_idx";
@@ -77,17 +77,17 @@ export async function down({ db, payload, req }: MigrateDownArgs): Promise<void>
   ALTER TABLE "image_profiles" RENAME COLUMN "export_policy_original" TO "output_original";
   ALTER TABLE "_image_profiles_v" RENAME COLUMN "version_export_policy_original" TO "version_output_original";
   ALTER TABLE "image_profiles_output_allowed_formats" DROP CONSTRAINT "image_profiles_export_policy_allowed_formats_parent_fk";
-  
+
   ALTER TABLE "_image_profiles_v_version_output_allowed_formats" DROP CONSTRAINT "_image_profiles_v_version_export_policy_allowed_formats_parent_fk";
-  
+
   ALTER TABLE "graphic_profiles_output_allowed_formats" DROP CONSTRAINT "graphic_profiles_export_policy_allowed_formats_parent_fk";
-  
+
   ALTER TABLE "_graphic_profiles_v_version_output_allowed_formats" DROP CONSTRAINT "_graphic_profiles_v_version_export_policy_allowed_formats_parent_fk";
-  
+
   ALTER TABLE "templates_output_allowed_formats" DROP CONSTRAINT "templates_export_policy_allowed_formats_parent_fk";
-  
+
   ALTER TABLE "_templates_v_version_output_allowed_formats" DROP CONSTRAINT "_templates_v_version_export_policy_allowed_formats_parent_fk";
-  
+
   DROP INDEX "image_profiles_export_policy_allowed_formats_order_idx";
   DROP INDEX "image_profiles_export_policy_allowed_formats_parent_idx";
   DROP INDEX "_image_profiles_v_version_export_policy_allowed_formats_order_idx";
